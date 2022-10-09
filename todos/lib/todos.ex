6d8efde1526ddef36bc618cf45f88913da386a19
Vikram <vikram.aroskar@gmail.com>
2022-10-09 14:23:32 +0530

    hello world

---
+++ todos/lib/todos.ex
@@ -0,0 +1,19 @@
+defmodule Todos do
+  @moduledoc """
+  Documentation for `Todos`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Todos.hello()
+      :world
+
+  """
+  def hello do
+    IO.puts(:stdio, "Hello world")
+    :world
+  end
+end
